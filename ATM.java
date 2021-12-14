022f6f5c19689089f68bd8ee42b8d15e85958547
Alex7225 <oleksandr_grigor@ukr.net>
2021-12-14 22:03:48 +0200

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,32 @@
+public class ATM {
+    public int countBanknotes(int sum){
+        int i=0;
+        while (sum>=500){
+            sum=sum-500; i++;
+        }
+        while (sum>=200){
+            sum=sum-200; i++;
+        }
+        while (sum>=100){
+            sum=sum-100; i++;
+        }
+        while (sum>=50){
+            sum=sum-50; i++;
+        }
+        while (sum>=20){
+            sum=sum-20; i++;
+        }
+        while (sum>=10){
+            sum=sum-10; i++;
+        }
+        while (sum>=5){
+            sum=sum-5; i++;
+        }
+        while (sum>=2){
+            sum=sum-2; i++;
+        }
+        while (sum>=1){
+            sum=sum-1; i++;
+        } return i;
+    }
+}
